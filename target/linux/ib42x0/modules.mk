e297aa30aaf98429b4e67986167a66bf202c8647
Tobias Waldvogel <tobias.waldvogel@trustanywhere.com>
2013-02-22 21:12:38 +0100

    Release 20130222

--- target/linux/ib42x0/modules.mk
+++ target/linux/ib42x0/modules.mk
@@ -27,17 +27,11 @@ define KernelPackage/pata-gemini
   DEPENDS:=@TARGET_ib42x0
   KCONFIG:= CONFIG_PATA_GEMINI
   FILES:=$(LINUX_DIR)/drivers/ata/pata_gemini.ko
+  AUTOLOAD:=$(call AutoLoad,01,pata_gemini)
 endef
 
 define KernelPackage/pata-gemini/description
  PATA driver for gemini SoC
-endef
-
-define KernelPackage/pata-gemini/install
-	$(INSTALL_DIR) $(1)/etc/config
-	$(INSTALL_DATA) $(PLATFORM_DIR)/files/pata_gemini.config $(1)/etc/config/pata_gemini
-	$(INSTALL_DIR) $(1)/etc/init.d
-	$(INSTALL_BIN) $(PLATFORM_DIR)/files/pata_gemini.init $(1)/etc/init.d/pata_gemini
 endef
 
 $(eval $(call KernelPackage,pata-gemini))
